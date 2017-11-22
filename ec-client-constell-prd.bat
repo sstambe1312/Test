35dfe160c9ce3dbe75652fdb6947e35e280f231d
sstambe1312 <sachin_tambe2000@yahoo.com>
2017-11-22 12:31:04 -0600

    Ok

---
+++ ec-client-constell-prd.bat
@@ -0,0 +1,2 @@
+ecagent_windows_sys.exe -mod "client" -aid "FtW9Od" -cid "constellation_uaa_client" -csc "ConstellCl@123" -dur "10000" -hst "wss://constellation-ec-gateway.run.aws-usw02-pr.ice.predix.io/agent" -tid "LPWS0h" -oa2 "https://exelon-constellation-prd.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token" -lpt "10000" -dbg -zon "25d5e67a-3ddf-47e8-9aad-43cec724dc13"
+
